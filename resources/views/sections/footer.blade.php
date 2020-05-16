4b565409049fcc7063bcd2e016eaf9fa67101c75
Espiridion Larosa <edion.larosa@gmail.com>
2020-05-16 08:14:16 +0800

    update footer - add powered by

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,10 @@
 <footer id="footer">
+    <div class="footer-left">
+        <p>Powered by:
+            <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i></a>
+            and
+            <a href="https://github.com/actions" target="_blank"><i class="fab fa-github"></i></a>
+        </p>
+    </div>
     @include('sections.nav')
 </footer>
