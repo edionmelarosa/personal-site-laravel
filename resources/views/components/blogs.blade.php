33d56e3a71052f9661bfef6a5198986154bf02a2
Espiridion Larosa <edion.larosa@gmail.com>
2020-06-01 00:14:37 +0800

    updat blog

--- resources/views/components/blogs.blade.php
+++ resources/views/components/blogs.blade.php
@@ -7,7 +7,7 @@
                 $blog = (object) $blog;
             @endphp
             <a class="text-gray-800" href="{{url(\App\Blog\Blog::blogUrl($blog->slug))}}">
-                <div class="my-6 pb-5 border-t">
+                <div class="py-6 pb-5 border-t">
                     <span class="font-semibold text-2xl block hover:text-blue-600">{{$blog->title}}</span>
                     <span class="text-xl mb-1 block">{{$blog->excerpt}}</span>
                     <small class="text-sm font-gray-600 block">Updated: {{$blog->updated_at->format('m d, Y')}}</small>
