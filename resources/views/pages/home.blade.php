f09a653dcc29a656fd6f3d4509f80e3dc23641f0
Espiridion Larosa <edion.larosa@gmail.com>
2021-02-10 12:54:34 +0800

    remove community posts section in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -45,36 +45,5 @@
         </div>
     </section>
 
-    <section class="blog-section px-3 py-10 bg-gray-300">
-        <div class="text-center mb-10">
-            <h2 class="font-bold md:text-4xl mb-0 mt-0">Community Posts</h2>
-        </div>
-        <div class="md:px-10 max-w-6xl m-auto">
-            <div class="md:flex content-center">
-                <div class="bg-gray-200 mb-3 md:mr-3 p-6 py-6 rounded-sm shadow-md md:w-2/3 text-center">
-                    <a class="text-gray-800" 
-                        href="https://freek.dev/1676-adding-trycatch-to-laravel-collections" 
-                        target="_blank">
-                        <span class="block hover:text-blue-600">Adding try/catch to Laravel collections</span>
-                    </a>
-                </div>
-                <div class="bg-gray-200 mb-3 md:mr-3 p-6 py-6 rounded-sm shadow-md md:w-2/3 text-center">
-                    <a class="text-gray-800" 
-                    href="https://divinglaravel.com/when-does-php-call-__destruct" 
-                    target="_blank">
-                    <span class="block hover:text-blue-600">When does PHP call __destruct()?</span>
-                    </a>
-                </div>
-                <div class="bg-gray-200 mb-3 md:mr-3 p-6 py-6 rounded-sm shadow-md md:w-2/3 text-center">
-                    <a class="text-gray-800" 
-                        href="https://driesvints.com/blog/the-beauty-of-single-action-controllers/" 
-                        target="_blank">
-                    <span class="block hover:text-blue-600">The Beauty of Single Action Controllers</span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-
     @include('components.client-reviews')
 @endsection
