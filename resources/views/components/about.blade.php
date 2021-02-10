c0ab64ed9feaa558050762e0845f50ff859bafe5
Espiridion Larosa <edion.larosa@gmail.com>
2021-02-10 11:29:41 +0800

    Update about.blade.php

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -9,11 +9,7 @@ class="px-3 md:px-16flex-auto pt-10 mb-5 text-xl">
     </div> --}}
     <div class="text-xl px-5 md:pt-4 sm:pt-10">
         <h1 class="font-bold text-4xl mb-4 ">Hello!</h1>
-        <p>I am Espiridion Larosa Jr. Full stack developer from Philippines.</p>
-        <p>For the past 6 years I was working with several private companies.</p>
-        <p class="mt-3">
-            Aside from development, I love outdoor activities.
-        </p>
+        <p>I am Dion Larosa a full-stack developer. Currently working at <a href="https://www.skylinedynamics.com/" target="_blank">Skyline Dynamics</a> as senior API developer.</p>
     </div>
 </div>
 </section>
