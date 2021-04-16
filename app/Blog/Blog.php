36f52c7d1a6919103fdd5c8f033bfb7bebd40b3e
Espiridion Larosa <edion.larosa@gmail.com>
2021-04-16 10:04:30 +0800

    Fix blog

--- app/Blog/Blog.php
+++ app/Blog/Blog.php
@@ -18,30 +18,27 @@ class Blog
     public static function all()
     {
         if (!config('cache.enabled')) {
-            return WinkPost::limit(3)->get()
-                ->sortByDesc('created_at');
+            return WinkPost::orderByDesc('updated_at')->get();
         }
 
         return Cache::rememberForever(Blog::$cachePrefix . 'all', function () {
-            return WinkPost::limit(3)->get()
-                ->sortByDesc('created_at');
+            return WinkPost::orderByDesc('updated_at')
+                ->get();
         });
     }
 
-    public static function limit(string $limit)
+    public static function limit(int $limit = 6)
     {
-        if (!$limit) {
-            throw new \Exception("Limit is required", 1);
-        }
-
         if (!config('cache.enabled')) {
-            return WinkPost::limit($limit)->get()
-                ->sortByDesc('created_at');
+            return WinkPost::orderByDesc('created_at')
+                ->limit($limit)
+                ->get();
         }
 
         return Cache::rememberForever(Blog::$cacheLimitPrefix . $limit, function () use ($limit) {
-            return WinkPost::limit($limit)->get()
-            ->sortByDesc('created_at');
+            return WinkPost::orderByDesc('created_at')
+                ->limit($limit)
+                ->get();
         });
     }
 
